6676bd979fd3ccaf74a9fa5e8b980e3d57025819
JunSeo <oponize@naver.com>
2021-07-01 15:30:36 +0900

    인트로 페이지 최종 수정

--- react/quize_site/src/pages/IntroPage.js
+++ react/quize_site/src/pages/IntroPage.js
@@ -1,19 +1,49 @@
 import React from 'react';
 import styled from 'styled-components';
+import { BlueButton } from '../components/BlueButton';
+import { useDispatch } from 'react-redux';
+import { next_page } from '../store/modules/score';
 
 const IntroDiv = styled.div`
 	width: 100%;
 	display: flex;
 	flex-direction: column;
 	align-items: center;
+
+	h1 {
+		margin-top: 3rem;
+		font-weight: bold;
+	}
+
+	h3 {
+		margin-bottom: 2rem;
+	}
+
+	img {
+		border-radius: 1em;
+		margin: 3rem 0;
+	}
+
+	@media screen and (max-width: 400px) {
+		img {
+			width: 300px;
+			height: 300px;
+		}
+	}
 `;
 
 const IntroPage = () => {
+	const dispatch = useDispatch();
 	return (
 		<IntroDiv>
-			<img alt="메인이미지" src="./인트로.gif" />
 			<h1>축구선수 이름 퀴즈!</h1>
-			<h2>과연 다 맞출수 있을까..?!!</h2>
+			<img alt="메인이미지" src="/images/intro.gif" />
+			<h3>과연 다 맞출수 있을까..?!!</h3>
+
+			<BlueButton
+				text="테스트 시작!"
+				clickEvent={() => dispatch(next_page())}
+			/>
 		</IntroDiv>
 	);
 };

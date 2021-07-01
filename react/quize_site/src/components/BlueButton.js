11c5a4064111d7e343e696281d3e0eae61ddba93
JunSeo <oponize@naver.com>
2021-07-01 15:30:12 +0900

    버튼 컴포넌트 최종 수정

--- react/quize_site/src/components/BlueButton.js
+++ react/quize_site/src/components/BlueButton.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Button } from 'react-bootstrap';
 
-const ButtonBlue = styled.a``;
+const ButtonBlue = styled(Button)`
+	width: 175px;
+	margin: 1rem 0;
+	font-weight: bold;
 
-const BlueButton = ({ text, clickEvent }) => {
-	return <ButtonBlue onClick={clickEvent}>{text}</ButtonBlue>;
-};
+	@media screen and (max-width: 400px) {
+		font-size: medium;
+	}
+`;
 
-export default BlueButton;
+export function BlueButton({ text, clickEvent }) {
+	return (
+		<>
+			<ButtonBlue href="#" size="lg" onClick={clickEvent}>
+				{text}
+			</ButtonBlue>
+		</>
+	);
+}

d3833350ec31cf77335e2d78b5b9002ac82dd0b4
JunSeo <oponize@naver.com>
2021-07-01 15:30:59 +0900

    퀴즈 페이지 최종 수정

--- react/quize_site/src/pages/QuizePage.js
+++ react/quize_site/src/pages/QuizePage.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 import { BlueButton } from '../components/BlueButton';
 import { next_page, check_correct } from '../store/modules/score';
 import { useDispatch } from 'react-redux';
+import ProgressBar from 'react-bootstrap/ProgressBar';
 
 const Quizediv = styled.div`
 	width: 100%;
@@ -22,9 +23,20 @@ const Quizediv = styled.div`
 	}
 `;
 
+const Progress = styled(ProgressBar)`
+	width: 35em;
+
+	margin-top: 2.5rem;
+
+	@media screen and (max-width: 400px) {
+		width: 250px;
+	}
+`;
+
 const PlayerImage = styled.img``;
 const QuizePage = ({ page, quize }) => {
 	const dispatch = useDispatch();
+	const now = page * 5;
 	const setImage = useCallback((page) => {
 		const src = `/images/${page}.png`;
 
@@ -40,22 +52,27 @@ const QuizePage = ({ page, quize }) => {
 	);
 
 	return (
-		<Quizediv>
-			<h4>{quize[page - 1].q}</h4>
-			<PlayerImage alt="축구선수" src={setImage(page)} />
-			<BlueButton
-				text={quize[page - 1].a[0].player}
-				clickEvent={() => setQuize(0)}
-			/>
-			<BlueButton
-				text={quize[page - 1].a[1].player}
-				clickEvent={() => setQuize(1)}
-			/>
-			<BlueButton
-				text={quize[page - 1].a[2].player}
-				clickEvent={() => setQuize(2)}
-			/>
-		</Quizediv>
+		<>
+			{page < 21 && (
+				<Quizediv>
+					<h4>{quize[page - 1].q}</h4>
+					<PlayerImage alt="축구선수" src={setImage(page)} />
+					<BlueButton
+						text={quize[page - 1].a[0].player}
+						clickEvent={() => setQuize(0)}
+					/>
+					<BlueButton
+						text={quize[page - 1].a[1].player}
+						clickEvent={() => setQuize(1)}
+					/>
+					<BlueButton
+						text={quize[page - 1].a[2].player}
+						clickEvent={() => setQuize(2)}
+					/>
+					<Progress now={now} animated label={`${page}/20`} />
+				</Quizediv>
+			)}
+		</>
 	);
 };
 
